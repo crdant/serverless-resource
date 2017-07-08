f55de64b2539bb9b5b791e8b22eed4cd8645623f
Chuck D'Antonio <c_dantonio@acm.org>
2017-07-08 12:15:25 -0400

    Stylistic changes

--- bin/build.sh
+++ bin/build.sh
@@ -3,19 +3,19 @@
 stub=$1; shift
 set -e
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
 export ATC_URL=${ATC_URL:-"http://192.168.100.4:8080"}
-export fly_target=${fly_target:-tutorial}
+export fly_target=${fly_target:-lite}
 echo "Concourse API target ${fly_target}"
 echo "Concourse API $ATC_URL"
-echo "Tutorial $(basename $DIR)"
 
 realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
 usage() {
-  echo "USAGE: run.sh path/to/credentials.yml"
+  me=$(basename ${0})
+  echo "USAGE: ${me} path/to/credentials.yml"
   exit 1
 }
 
@@ -30,8 +30,8 @@ fi
 
 
 pushd $DIR
-  fly sp -t ${fly_target} configure -c ci/pipeline.yml -p serverless-resource --load-vars-from ${stub} -n
+  fly -t ${fly_target} set-pipeline -p serverless-resource --config ci/pipeline.yml --load-vars-from ci/properties.yml --load-vars-from ${stub} -n
   fly -t ${fly_target} unpause-pipeline --pipeline serverless-resource
-  fly -t ${fly_target} trigger-job -j serverless-resource/job-publish
-  fly -t ${fly_target} watch -j serverless-resource/job-publish
+  fly -t ${fly_target} trigger-job -j serverless-resource/publish
+  fly -t ${fly_target} watch -j serverless-resource/publish
 popd
