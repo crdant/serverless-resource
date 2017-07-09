94656a51467d8adab271b72ead2d75971d8069c2
Chuck D'Antonio <c_dantonio@acm.org>
2017-07-09 08:40:10 -0400

    Added code to read expected source values

--- assets/common.sh
+++ assets/common.sh
@@ -1,8 +1,18 @@
-serverless_cmd() {
-  GREEN='\033[0;32m'
-  RED='\033[0;31m'
-  NC='\033[0m' # No Color
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
+store_payload() {
+  payload=$(mktemp /tmp/resource-in.XXXXXX)
+  cat > $payload <&0
+}
+
+extract_parameter(){
+  parameter="${1}"
+  jq -r '.source.{$parameter} // ""' < "${payload}"
+}
+
+serverless_cmd() {
   cmd="serverless $@"
 
   printf "Running serverless command: ${GREEN}serverless %s${NC}" "$@"
